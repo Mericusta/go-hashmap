28265cff6cc4ea3abf0a6f6b4814f37b6855b646
yunpeng.li <yunpeng.li@vaneplus.com>
2021-11-24 15:03:01 +0800

    #202111241502 refactor HashMap.array to HashMap.data with interface HashMapData

--- main.go
+++ main.go
@@ -20,28 +20,121 @@ func (h *HashValue) equal(k int) bool {
 	return h.k == k
 }
 
-type HashMap struct {
-	loadFactor float64      // allocator
-	useCount   uint         // allocator
-	array      []*HashValue // data structure
-	hashFunc   func(int, int) int
-	collision  func(int, func(*HashValue) bool, []*HashValue) int
-}
-
 func defaultHashFunc(k, l int) int {
 	return k & (l - 1)
 }
 
-func defaultCollision(i int, compareFunc func(*HashValue) bool, a []*HashValue) int {
-	if i < 0 || len(a) <= i {
+func defaultCollision(i int, compareFunc func(*HashValue) bool, d HashMapData) int {
+	if i < 0 || d.Len() <= i {
 		return -1
 	}
-	for index := i; index < len(a); index++ {
-		if compareFunc(a[index]) {
+	for index := i; index < d.Len(); index++ {
+		if compareFunc(d.Get(index)) {
 			return index
 		}
 	}
 	return -1
+}
+
+type HashMapData interface {
+	Len() int
+	Set(int, *HashValue)
+	Get(int) *HashValue
+	GetV(int) int
+	SetV(int, int)
+	Reallocate(uint)
+}
+
+func MakeHashMapData(d HashMapData) HashMapData {
+	return d
+}
+
+type ArrayHashMapData struct {
+	array []*HashValue
+}
+
+func (d *ArrayHashMapData) Len() int {
+	return len(d.array)
+}
+
+func (d *ArrayHashMapData) Set(i int, v *HashValue) {
+	d.array[i] = v
+}
+
+func (d *ArrayHashMapData) Get(i int) *HashValue {
+	return d.array[i]
+}
+
+func (d *ArrayHashMapData) GetV(i int) int {
+	return d.array[i].v
+}
+
+func (d *ArrayHashMapData) SetV(i int, v int) {
+	d.array[i].v = v
+}
+
+func (d *ArrayHashMapData) Reallocate(size uint) {
+	if uint(len(d.array)) != size {
+		d.array = make([]*HashValue, size)
+	}
+}
+
+type HashMap struct {
+	loadFactor float64     // allocator
+	useCount   uint        // allocator
+	data       HashMapData // data structure
+	hashFunc   func(int, int) int
+	collision  func(int, func(*HashValue) bool, HashMapData) int
+}
+
+func (h *HashMap) Set(k, v int) (int, bool) {
+	hashIndex := h.collision(h.hashFunc(k, h.data.Len()), func(h *HashValue) bool {
+		return h == nil
+	}, h.data)
+	if hashIndex == -1 {
+		return hashIndex, false
+	}
+
+	if h.GetLoadFactor(1) >= h.loadFactor {
+		fmt.Printf("HashMap should reallocate\n")
+	}
+
+	h.data.Set(hashIndex, &HashValue{
+		k: k,
+		v: v,
+	})
+	h.useCount++
+	return hashIndex, true
+}
+
+func (h *HashMap) Get(k int) (int, bool) {
+	hashIndex := h.collision(h.hashFunc(k, h.data.Len()), func(h *HashValue) bool {
+		return h != nil && h.equal(k)
+	}, h.data)
+	if hashIndex == -1 {
+		return hashIndex, false
+	}
+	return h.data.GetV(hashIndex), true
+}
+
+func (h *HashMap) Del(k int) (int, bool) {
+	hashIndex := h.collision(h.hashFunc(k, h.data.Len()), func(h *HashValue) bool {
+		return h != nil && h.equal(k)
+	}, h.data)
+	if hashIndex == -1 {
+		return 0, false
+	}
+	v := h.data.GetV(hashIndex)
+	h.data.SetV(hashIndex, 0)
+	h.useCount--
+	return v, true
+}
+
+func (h *HashMap) GetLoadFactor(delta uint) float64 {
+	if h.data.Len() == 0 {
+		return 0
+	}
+	return float64(h.useCount+delta) / float64(h.data.Len())
 }
 
 type HashMapOption func(*HashMap)
@@ -49,9 +142,11 @@ type HashMapOption func(*HashMap)
 func MakeHashMap(options ...HashMapOption) *HashMap {
 	hashMap := &HashMap{
 		loadFactor: DEFAULT_LOAD_FACTOR,
-		array:      make([]*HashValue, DEFAULT_HASH_MAP_SIZE),
-		hashFunc:   defaultHashFunc,
-		collision:  defaultCollision,
+		data: &ArrayHashMapData{
+			array: make([]*HashValue, DEFAULT_HASH_MAP_SIZE),
+		},
+		hashFunc:  defaultHashFunc,
+		collision: defaultCollision,
 	}
 	for _, option := range options {
 		option(hashMap)
@@ -65,9 +160,17 @@ func WithHashMapLoadFactor(factor float64) HashMapOption {
 	}
 }
 
+func WithHashMapData(data HashMapData) HashMapOption {
+	return func(h *HashMap) {
+		h.data = data
+	}
+}
+
 func WithHashMapSize(size uint) HashMapOption {
 	return func(h *HashMap) {
-		h.array = make([]*HashValue, size)
+		if h.data != nil {
+			h.data.Reallocate(size)
+		}
 	}
 }
 
@@ -77,60 +180,10 @@ func WithHashMapHashFunc(f func(int, int) int) HashMapOption {
 	}
 }
 
-func WithHashMapCollision(f func(int, func(*HashValue) bool, []*HashValue) int) HashMapOption {
+func WithHashMapCollision(f func(int, func(*HashValue) bool, HashMapData) int) HashMapOption {
 	return func(h *HashMap) {
 		h.collision = f
 	}
-}
-
-func (h *HashMap) Set(k, v int) (int, bool) {
-	hashIndex := h.collision(h.hashFunc(k, len(h.array)), func(h *HashValue) bool {
-		return h == nil
-	}, h.array)
-	if hashIndex == -1 {
-		return hashIndex, false
-	}
-
-	if h.GetLoadFactor(1) >= h.loadFactor {
-		fmt.Printf("HashMap should reallocate\n")
-	}
-
-	h.array[hashIndex] = &HashValue{
-		k: k,
-		v: v,
-	}
-	h.useCount++
-	return hashIndex, true
-}
-
-func (h *HashMap) Get(k int) (int, bool) {
-	hashIndex := h.collision(h.hashFunc(k, len(h.array)), func(h *HashValue) bool {
-		return h != nil && h.equal(k)
-	}, h.array)
-	if hashIndex == -1 {
-		return hashIndex, false
-	}
-	return h.array[hashIndex].v, true
-}
-
-func (h *HashMap) Del(k int) (int, bool) {
-	hashIndex := h.collision(h.hashFunc(k, len(h.array)), func(h *HashValue) bool {
-		return h != nil && h.equal(k)
-	}, h.array)
-	if hashIndex == -1 {
-		return 0, false
-	}
-	v := h.array[hashIndex].v
-	h.array[hashIndex].v = 0
-	h.useCount--
-	return v, true
-}
-
-func (h *HashMap) GetLoadFactor(delta uint) float64 {
-	if len(h.array) == 0 {
-		return 0
-	}
-	return float64(h.useCount+delta) / float64(len(h.array))
 }
 
 func main() {
